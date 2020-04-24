eb09797a97096e4a1d4101178259396c401b50a3
tangch <tangch@yuanian.com>
2020-04-24 11:17:45 +0800

    completableFutre test

--- src/main/java/com/yvhkfhvk/java8/concurrent/CompletableFutureTest.java
+++ src/main/java/com/yvhkfhvk/java8/concurrent/CompletableFutureTest.java
@@ -4,6 +4,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -25,6 +27,7 @@ public class CompletableFutureTest {
 
     /**
      * 测试supplyAsync
+     * 执行并有返回值
      */
     public static void testSupplyAsync(){
         CompletableFuture.supplyAsync(()->{
@@ -39,8 +42,66 @@ public class CompletableFutureTest {
         });
     }
 
+    /**
+     * 线程合并执行
+     */
+    public static void testCombine(){
+        CompletableFuture<Integer> future1 = CompletableFuture.supplyAsync(() -> ThreadLocalRandom.current().nextInt());
+        CompletableFuture<Integer> future2 = CompletableFuture.supplyAsync(() -> ThreadLocalRandom.current().nextInt());
+        CompletableFuture<Integer> future3 = future1.thenCombine(future2, (t1, t2) -> {
+            logger.debug(t1 + "," + t2);
+            return t1 + t2;
+        });
+        try {
+            logger.debug(future3.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 测试thenapply
+     * 某个线程依赖另一个线程，让线程串行执行
+     */
+    public static void testThenApply() {
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> ThreadLocalRandom.current().nextInt());
+        future.thenApply(i ->
+                i + ThreadLocalRandom.current().nextInt()
+        );
+        try {
+            logger.debug(future.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 测试handle
+     * 与testThenApply ，不同在于出现异常也会执行
+     */
+    public static void testHandle(){
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> ThreadLocalRandom.current().nextInt());
+        future.handle((i,t) ->
+                i + ThreadLocalRandom.current().nextInt()
+        );
+        try {
+            logger.debug(future.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         testComplete();
         testSupplyAsync();
+        testCombine();
+        testThenApply();
+        testHandle();
     }
 }
